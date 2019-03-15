1c7abf61f86ee39f09e218fe8918e4f4d586b443
Jo <jonathan.jacquet62@laposte.net>
2019-03-15 15:34:58 +0100

    list customers

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-alert("test : ceci est un test");
+//Variables
+//get tbody by id
+  var tbody = document.getElementById("tbody");
+  // create object XMLHttpRequest
+  var xhttp = new XMLHttpRequest();
+  //function get JSON file
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var listCustomers = JSON.parse(this.responseText);
+      //stock board in var customers
+      var customers = listCustomers.customers;
+      //loop customers board for get object
+      for (let i = 0; i < customers.length; i++) {
+        var tr = document.createElement("tr");
+          tbody.appendChild(tr);
+        for(infos in customers[i])  {
+          var td = document.createElement("td");
+          tr.appendChild(td);
+          //fill td balise with array values
+          td.innerHTML = customers[i][infos];
+        }
+      }
+    }
+};
+
+//Functions
+
+
+
+
+
+
+//Code Logic
+xhttp.open("GET", "customers.json", true);
+xhttp.send();
